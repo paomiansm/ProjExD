971accc29478af7a22a1c651e89c70857bbc8253
Shu Sei <c0a2106663@edu.teu.ac.jp>
2022-06-14 16:01:00 +0900

    afa

---
+++ ex01/quzi.py
@@ -0,0 +1,25 @@
+import random
+import datetime
+st = datetime.datetime.now()
+ed = datetime.datetime.now()
+def main():
+    shutudai()
+    kaito()
+
+def shutudai():
+    listq = ["サザエの旦那の名前は？","カツオの妹の名前は？","クラオはカツオから見てどんな関係？"]
+    listn = random.randint(0,2)
+    print(listq[listn],listn)
+    return listn
+    
+
+def kaito():
+    s = input("答えるんだ：")
+    listk = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
+    if s in listk[shutudai()]:
+        print("正解!!!")
+        return True
+    else:
+        print("出直してこい")
+        return False
+main()
