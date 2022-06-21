39a2d469dfbb7573253b57b01f075fc96a8946d5
Shu Sei <c0a2106663@edu.teu.ac.jp>
2022-06-21 17:00:24 +0900

    試作電卓完成した

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,43 +1,70 @@
+from calendar import c
 import tkinter as tk
-from tkinter import font
 import tkinter.messagebox as tkm
-from tkinter.tix import COLUMN
-
+import math
 def button_click(event):
     btn = event.widget
-    num = btn["text"] # クリックされたボタンの文字
+    txt = btn["text"]
+    # tkm.showwarning("確認", 
+    #                  f"you clicked on {txt} button.")
+    if txt == "=":
+        keisan = entry.get() 
+        result = eval(keisan) 
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, result)
     
-    if num == "=":
-        eqn = engry.get()
-        res = eval(eqn)
-        engry.delete(0, tk.END)
-        engry.insert(tk.END, res)
-    else:
-        engry.insert(tk.END, num)
+    elif txt == "C":
+        entry.delete(0, tk.END) 
+    
+    elif txt == "Del":
+        n = len(entry.get())-1
+        entry.delete(n,tk.END)
+    
+    elif txt == "sqrt(x)":
+        keisan = entry.get() 
+        result = math.sqrt(int(keisan)) 
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, result)
 
+    elif txt == "X^2":
+        num = entry.get()
+        result=int(num)*int(num)
+        entry.delete(0, tk.END)   
+        entry.insert(tk.END, result)
+
+    elif txt == "+/-":
+         num = entry.get()
+         num1 = int(num)*-1
+         entry.delete(0, tk.END)
+         entry.insert(tk.END, num1)
+    else:
+        entry.insert(tk.END, txt)
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
-    root.geometry("300x550")
 
-    engry = tk.Entry(root,justify="right", width=10, font=("Times New Roman", 40))
-    engry.grid(row=0,column=0,columnspan=3) # 横方向に3マス接合
+    entry = tk.Entry(root, justify="right", width = 18, font=("Times New Roman",40))
+    entry.grid(row = 0, column= 0, columnspan=4)
 
-    r, c = 40,0      #    r:行番号　c:列番号
-    for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]):
-        btn = tk.Button(root, text=f"{num}", width=4,height=2,font=("Times New Roman", 30))
-        btn.bind("<1>", button_click)
-        btn.grid(row=r,column=c)
+    r, c = 40, 0
+    for i,num in enumerate(["%","CE","C","Del","1/x","X^2","sqrt(x)","/",7,8,9,"*",4,5,6,"-",1,2,3,"+","+/-",0,",","="]):
+        button = tk.Button(root,
+                    width= 5, 
+                    height=1, 
+                    font=("Times New Roman", 30),
+                    text=f"{num}"
+                    )
+        button.grid(row=r, column=c)
         c += 1
-        if (i+1)%3 == 0:
+        if (i+1) % 4 == 0:
             r += 1
-            c = 0
-
-
-
-
-
+            c = 0  
+        button.bind("<1>", button_click)
+        
+        if num == "=":
+           button["bg"] = "DeepSkyBlue2"
 
     root.mainloop()
     
