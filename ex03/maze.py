9ec481be12a3406e3876e4b455c85e55f09f4453
Shu Sei <c0a2106663@edu.teu.ac.jp>
2022-06-28 14:32:40 +0900

    No.5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,8 +1,23 @@
 import tkinter as tk
 from tkinter import Canvas as cav
+
+def key_down(event):
+    global key
+    key = event.keysym
+    print(key)
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+
+    tori = tk.PhotoImage(file="ex03/fig/5.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image= tori, tag= tori)
+
+    key = ""
+    root.bind("<KeyPress>", key_down)
+
     root.mainloop()
