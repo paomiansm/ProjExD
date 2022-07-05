5f69bdf9dc49f2ac6560c8ba0468900c1f15fb50
Shu Sei <c0a2106663@edu.teu.ac.jp>
2022-07-05 14:23:52 +0900

    練習３完成

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,6 +3,8 @@ import sys
 
 def main():
     clock = pg.time.Clock()
+
+#練習１
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900))
     screen_rct = screen_sfc.get_rect()
@@ -12,18 +14,25 @@ def main():
 
     #pg.display.update()
 
-    clock.tick(0.5)
+    #clock.tick(0.5)
+
+#練習３
+    kkimg_sfc = pg.image.load("ex04/fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900, 400
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
-        #練習２
+#練習２
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         pg.display.update()
         clock.tick(1000)
-        
+
 
 
 
