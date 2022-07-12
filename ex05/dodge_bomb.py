3c74c0213f9a96c1a6c2a138324880bc2c41fd5d
Shu Sei <c0a2106663@edu.teu.ac.jp>
2022-07-12 15:49:41 +0900

    3限のプログラム完成

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -3,100 +3,100 @@ import sys
 import random
 import datetime
 import tkinter.messagebox as tkm
+
+
+class Screen:
+    def __init__(self, title, wh, image):
+        pg.display.set_caption(title)
+        self.sfc = pg.display.set_mode(wh)
+        self.rct = self.sfc.get_rect()
+        self.bgi_sfc = pg.image.load(image)
+        self.bgi_rct = self.bgi_sfc.get_rect()
+
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+            
+
+class Bird:
+    def __init__(self, image: str, size: float, xy):
+        self.sfc = pg.image.load(image)
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy
+    
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+
+    def update(self, scr: Screen):
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP] :
+            self.rct.centery -= 1
+        if key_states[pg.K_DOWN] :
+            self.rct.centery += 1
+        if key_states[pg.K_LEFT] :
+            self.rct.centerx -= 1
+        if key_states[pg.K_RIGHT]:
+            self.rct.centerx += 1
+        if check_bound(self.rct, scr.rct) != (1, 1):
+            if key_states[pg.K_UP] == True: self.rct.centery += 1
+            if key_states[pg.K_DOWN] == True: self.rct.centery -= 1
+            if key_states[pg.K_LEFT] == True: self.rct.centerx += 1
+            if key_states[pg.K_RIGHT] == True: self.rct.centerx -= 1
+        self.blit(scr)
+
+
+
+class Bomb:
+    def __init__(self, color, size, vxy, scr: Screen):
+        self.sfc = pg.Surface((2*size, 2*size))
+        self.sfc.set_colorkey((0,0,0))
+        pg.draw.circle(self.sfc, color, (size, size), size)
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = random.randint(0, scr.rct.width)
+        self.rct.centery = random.randint(0, scr.rct.height)
+        self.vx, self.vy = vxy
+
+    def blit(self, scr: Screen):
+        self.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr: Screen):
+        
+        self.rct.move_ip(self.vx, self.vy)
+        # screen_sfc.blit(bmimg_sfc, bmimg_rct)
+
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+
+        scr.sfc.blit(self.sfc, self.rct)
+        self.blit(scr)
+
 def main():
     clock = pg.time.Clock()
-    font = pg.font.Font(None, 60)
+
+    scr = Screen("逃げろ！こうかとん", (1600, 900), "ex04/fig/pg_bg.jpg")
+
+    kkt = Bird("ex05/fig/6.png",2.0,(900,400))
 
 
-#練習１
-    pg.display.set_caption("逃げろ！こうかとん")
-    screen_sfc = pg.display.set_mode((1600, 900))
-    screen_rct = screen_sfc.get_rect()
-    bgimg_sfc = pg.image.load("ex04/fig/pg_bg.jpg")
-    bgimg_rct = bgimg_sfc.get_rect()
-    speed = 1
-
-#練習３
-    kkimg_sfc = pg.image.load("ex04/fig/6.png")
-    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
-    kkimg_rct = kkimg_sfc.get_rect()
-    kkimg_rct.center = 900, 400
-
-#練習5
-
-    bmimg_sfc = pg.Surface((20, 20))
-    bmimg_sfc.set_colorkey((0,0,0))
-
-    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
-    bmimg_rct = bmimg_sfc.get_rect()
-    bmimg_rct.centerx = random.randint(0, screen_rct.width)
-    bmimg_rct.centery = random.randint(0, screen_rct.height)
-    vx, vy = +1, +1
+    bkd = Bomb((255,0,0), 10, (+1, +1), scr)
 
     while True:
-        screen_sfc.blit(bgimg_sfc, bgimg_rct)
-        screen_sfc.blit(kkimg_sfc, kkimg_rct)       
-        日付 = font.render(str(datetime.date.today()), True, (100, 0, 100))
-        時刻 = font.render(datetime.datetime.now().strftime("%H:%M:%S"), True, (0, 0, 100))
-        screen_sfc.blit(日付, [1350, 40])    
-        screen_sfc.blit(時刻, [1370, 80])
-#練習２
+        scr.blit()
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-#練習４
-        
-        key_states = pg.key.get_pressed()
-        if key_states[pg.K_UP] == True: kkimg_rct.centery -= speed
-        if key_states[pg.K_DOWN] == True: kkimg_rct.centery += speed
-        if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -= speed
-        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += speed
+        kkt.update(scr)
 
-        if check_bound(kkimg_rct, screen_rct) != (1, 1):
-            if key_states[pg.K_UP] == True: kkimg_rct.centery += speed
-            if key_states[pg.K_DOWN] == True: kkimg_rct.centery -= speed
-            if key_states[pg.K_LEFT] == True: kkimg_rct.centerx += speed
-            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= speed
-
-        screen_sfc.blit(kkimg_sfc,kkimg_rct)
-
-        bmimg_rct.move_ip(vx,vy)
-
-        screen_sfc.blit(bmimg_sfc, bmimg_rct)
-
-        yoko, tate = check_bound(bmimg_rct, screen_rct)
-        vx *= yoko
-        vy *= tate
-
-        if key_states[pg.K_SPACE] == True:
-            kkimg_sfc = pg.image.load("ex04/fig/3.png")
-            kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
-        else:
-            kkimg_sfc = pg.image.load("ex04/fig/6.png")
-            kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
-
-        if key_states[pg.K_LSHIFT] == True:
-            speed = 2
-        else:
-            speed = 1
-
-        if key_states[pg.K_1] == True:
-            kkimg_sfc = pg.image.load("ex04/fig/2.png")
-            kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
-
-
-        if kkimg_rct.colliderect(bmimg_rct):
-            if key_states[pg.K_SPACE] == True:
-                continue
-            if key_states[pg.K_1] == True:
-                vx *= -1
-                vy *= -1
-            else:
-                return False
+        bkd.update(scr)
+        if kkt.rct.colliderect(bkd.rct):
+            return
 
 
         pg.display.update()
         clock.tick(1000)
+
 
 def check_bound(rct, scr_rct):
 
